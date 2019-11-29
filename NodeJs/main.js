b98dbe96556b84755d921c8e16886ba19039b9bd
swapnilsarwade <swapnilsarwade@swapnils-air>
2019-11-29 23:24:32 +0530

    public resource added in node app

--- NodeJs/main.js
+++ NodeJs/main.js
@@ -1,11 +1,7 @@
-var http = require('http');
-var url = require('url');
-
-var server = http.createServer(function (req, res) {
-	res.writeHead(200, {'Content-Type': 'text/html'});
-	var q = url.parse(req.url, true).query;
-	var txt = q.year + " " + q.month;
-	res.end(txt);
-}).listen(8080);
-
-console.log("server is running on port 8080");
+const express = require('express');
+var app = express();
+app.use(express.static(__dirname + '/public'));
+app.get('/', (req, res) => {
+    res.send('Hello Express')
+});
+app.listen(process.env.PORT || 3000)
